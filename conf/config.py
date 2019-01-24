a21d05f1fd0bb889ff2a70d803649674d0ad80e3
C.Madhankumar <rcmadhankumar@gmail.com>
2019-01-24 00:38:51 -0500

    changing url's

--- conf/config.py
+++ conf/config.py
@@ -14,16 +14,16 @@ oozie_app_directory = "/oozie_data/app_directory"
 workflow_path = "src/workflow.xml"
 query_path = "src/hive.sql"
 job_file_path = "src/job.properties"
-oozie_interval = 15
+oozie_interval = 20
 
-url_list = ['http://download.cms.gov/openpayments/PGYR13_P062918.ZIP', 'http://download.cms.gov/openpayments/PGYR14_P062918.ZIP','http://download.cms.gov/openpayments/PGYR15_P062918.ZIP', 'http://download.cms.gov/openpayments/PGYR16_P062918.ZIP', 'http://download.cms.gov/openpayments/PGYR17_P062918.ZIP' ]
+url_list = ['http://download.cms.gov/openpayments/PGYR13_P011819.ZIP', 'http://download.cms.gov/openpayments/PGYR14_P011819.ZIP', 'http://download.cms.gov/openpayments/PGYR15_P011819.ZIP', 'http://download.cms.gov/openpayments/PGYR16_P011819.ZIP', 'http://download.cms.gov/openpayments/PGYR17_P011819.ZIP' ]
 
 spark2_home ="/usr/hdp/2.6.4.0-91/spark2/bin"
 schema_path_one = "/spark_job_data/src/rpay/schema1"
 schema_path_two = "/spark_job_data/src/rpay/schema2"
 spark_output_path = "/spark_job_data/out/rpay"
-spark_dir_list = ['/spark_job_data', '/spark_job_data/src', '/spark_job_data/src/rpay', '/spark_job_data/src/rpay/schema1', '/spark_job_data/src/rpay/schema2', '/spark_job_data/out', '/spark_job_data/out/rpay', 'user', 'user/root']
-spark_interval = 5
+spark_dir_list = ['/spark_job_data', '/spark_job_data/src', '/spark_job_data/src/rpay', '/spark_job_data/src/rpay/schema1', '/spark_job_data/src/rpay/schema2', '/spark_job_data/out', '/spark_job_data/out/rpay', '/user', '/user/root']
+spark_interval = 10
 create_parquet_script = "src/create_parq_common.py"
 spark_query_script = "src/query.py"
 
